0e61b97566048c625cd74268d5fc8f33625c6489
zayyo123 <764439554@qq.com>
2025-05-03 23:34:01 +0800

    refactor: 更新tsconfig.json和index.ts文件，添加详细注释以提升代码可读性，优化静态方法导出

--- packages/message/src/index.ts
+++ packages/message/src/index.ts
@@ -8,18 +8,23 @@ import {
   App,
 } from "antd";
 
+// 声明message、notification、modal变量
 let message: MessageInstance = antdMessage;
 let notification: NotificationInstance = antdNotification;
 
+// 获取antdModal中的所有静态方法，并去掉warn方法
 const { ...resetFns } = antdModal;
 let modal: Omit<ModalStaticFunctions, "warn"> = resetFns;
 
 /**
  * 该组件提供静态方法
+ * 把antd的message、notification、modal静态方法挂载到App上
  */
 function StaticMessage() {
+  // 使用App.useApp()获取静态方法
   const staticFunctions = App.useApp();
 
+  // 将获取到的静态方法赋值给message、notification、modal变量
   message = staticFunctions.message;
   notification = staticFunctions.notification;
   modal = staticFunctions.modal;
@@ -27,10 +32,8 @@ function StaticMessage() {
   return null;
 }
 
-export {
-  message,
-  notification,
-  modal,
-};
+// 导出message、notification、modal变量
+export { message, notification, modal };
 
+// 导出StaticMessage组件
 export default StaticMessage;
