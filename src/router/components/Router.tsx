369856f03b46006fb56f7f1ff4141bde251d0fef
zayyo123 <764439554@qq.com>
2025-05-05 20:54:21 +0800

    refactor: 统一代码风格，调整导入顺序和格式，提升可读性

--- src/router/components/Router.tsx
+++ src/router/components/Router.tsx
@@ -3,34 +3,38 @@ import type { DefaultComponent } from "@loadable/component";
 import { useEffect } from "react";
 import { handleRoutes } from "../utils/helper";
 import { useLocation, useRoutes } from "react-router-dom";
-import Login from '@/pages/login';
-import Forget from '@/pages/forget';
-import NotFound from '@/pages/404';
-import nprogress from 'nprogress';
-import Guards from './Guards';
+import Login from "@/pages/login";
+import Forget from "@/pages/forget";
+import NotFound from "@/pages/404";
+import nprogress from "nprogress";
+import Guards from "./Guards";
 
+// 定义页面文件类型
 type PageFiles = Record<string, () => Promise<DefaultComponent<unknown>>>;
-const pages = import.meta.glob('../../pages/**/*.tsx') as PageFiles;
+// 导入页面文件
+const pages = import.meta.glob("../../pages/**/*.tsx") as PageFiles;
+// 处理路由
 const layouts = handleRoutes(pages);
 
+// 定义路由对象
 const newRoutes: RouteObject[] = [
   {
     path: "login",
-    element: <Login />
+    element: <Login />,
   },
   {
     path: "forget",
-    element: <Forget />
+    element: <Forget />,
   },
   {
     path: "",
     element: <Guards />,
-    children: layouts
+    children: layouts,
   },
   {
     path: "*",
     element: <NotFound />,
-  }
+  },
 ];
 
 function App() {
@@ -49,11 +53,7 @@ function App() {
     };
   }, [location]);
 
-  return (
-    <>
-      { useRoutes(newRoutes) }
-    </>
-  );
+  return <>{useRoutes(newRoutes)}</>;
 }
 
 export default App;
